bc0b8052b9dc844cba80747e01cb3475feaa1d6b
vikassandhu999 <vikassandhu999@gmail.com>
2021-08-01 07:35:02 +0530

    refac : extract variables

--- donut.c
+++ donut.c
@@ -5,32 +5,12 @@
 static char *const shade_chars = ".,-~:;=!*#$@";
 static char *const clear_screen_chars = "\x1b[2J";
 static char *const set_cursor_to_start_chars = "\x1b[H";
-const int height = 22;
-const int width = 80;
 const char space = ' ';
 const char new_line = '\n';
+
+const int height = 22;
+const int width = 80;
 const int buffer_size = width * height;
-
-void next_frame(float A, float B, char *buffer, float *z);
-
-void display_frame(const char *buffer);
-
-int main() {
-    float A = 0;
-    float B = 0;
-    char buffer[buffer_size];
-    float z[buffer_size];
-    printf(clear_screen_chars);
-    while(1) {
-        memset(buffer, space, buffer_size);
-        memset(z, 0, buffer_size * 4);
-        next_frame(A, B, buffer, z);
-        printf(set_cursor_to_start_chars);
-        display_frame(buffer);
-        A += 0.04;
-        B +=0.02;
-    }
-}
 
 void display_frame(const char *buffer) {
     for (int k = 0; buffer_size >= k; k++)
@@ -47,7 +27,7 @@ void next_frame(float A, float B, char *buffer, float *z) {
             float cos_phi = cos(phi);
             float cos_theta = cos(theta);
             float sin_theta = sin(theta);
-            float sin_A =sin(A);
+            float sin_A = sin(A);
             float cos_A = cos(A);
             float cos_B = cos(B);
             float sin_B = sin(B);
@@ -57,11 +37,31 @@ void next_frame(float A, float B, char *buffer, float *z) {
             int x = 40 + 30 * D * (cos_phi * h * cos_B - t * sin_B);
             int y = 12 + 15 * D * (cos_phi * h * sin_B + t * cos_B);
             int o = x + width * y;
-            int N =  8 * ((sin_theta * sin_A - sin_phi * cos_theta * cos_A) * cos_B - sin_phi * cos_theta * sin_A - sin_theta * cos_A - cos_phi * cos_theta * sin_B);
+            int N = 8 * ((sin_theta * sin_A - sin_phi * cos_theta * cos_A) * cos_B - sin_phi * cos_theta * sin_A -
+                         sin_theta * cos_A - cos_phi * cos_theta * sin_B);
             if (height > y && y > 0 && x > 0 && width > x && D > z[o]) {
                 z[o] = D;
                 buffer[o] = shade_chars[N > 0 ? N : 0];
             }
         }
+    }
+}
+
+int main() {
+    float A = 0;
+    float B = 0;
+    double delta_A = 0.04;
+    double delta_B = 0.02;
+    char buffer[buffer_size];
+    float z[buffer_size];
+    printf("%s", clear_screen_chars);
+    while (1) {
+        memset(buffer, space, buffer_size);
+        memset(z, 0, buffer_size * 4);
+        next_frame(A, B, buffer, z);
+        printf("%s", set_cursor_to_start_chars);
+        display_frame(buffer);
+        A += delta_A;
+        B += delta_B;
     }
 }
