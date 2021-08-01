129ff0cf75e62f62f7337c5ca846fd9099790560
vikassandhu999 <vikassandhu999@gmail.com>
2021-08-01 06:23:06 +0530

    chore : start refactoring

---
+++ cmake-build-debug/CMakeFiles/RefactoringDonut.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RefactoringDonut.dir/donut.c.o"
+  "RefactoringDonut"
+  "RefactoringDonut.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RefactoringDonut.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
