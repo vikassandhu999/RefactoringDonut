52a6a29963d3d21b0afeca9f56d53db4e59b9f7e
vikassandhu999 <vikassandhu999@gmail.com>
2021-08-01 06:35:30 +0530

    refac: better formatt

--- donut.c
+++ donut.c
@@ -17,22 +17,25 @@ int main() {
         memset(z, 0, 7040);
         for (j = 0; 6.28 > j; j += 0.07)
             for (i = 0; 6.28 > i; i += 0.02) {
-                float c = sin(i), d = cos(j), e =
-                        sin(A), f = sin(j), g = cos(A), h = d + 2, D = 1 / (c *
-                                                                            h * e + f * g + 5), l = cos(i), m = cos(
-                        B), n = sin(B), t = c * h * g - f * e;
-                int x = 40 + 30 * D *
-                             (l * h * m - t * n), y = 12 + 15 * D * (l * h * n
-                                                                     + t * m), o = x + 80 * y, N =
-                        8 * ((f * e - c * d * g
-                             ) * m - c * d * e - f * g - l * d * n);
-                if (22 > y &&
-                    y > 0 && x > 0 && 80 > x && D > z[o]) {
+                float c = sin(i);
+                float d = cos(j);
+                float e =sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2, D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y = 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N =  8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
                     z[o] = D;;;
-                    b[o] =
-                            ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
                 }
-            }/*#****!!-*/
+            }
         printf("\x1b[H");
         for (k = 0; 1761 > k; k++)
             putchar(k % 80 ? b[k] : 10);
